b5a2f564ce363ab9705e0d00dcd336a56434fc1e
Muhammedys <engineer_mu@outlook.com>
2022-11-23 04:25:41 +0200

    working firebase 04:25

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,19 @@ import 'package:ecommerce_app/view/control_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'firebase_options.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
